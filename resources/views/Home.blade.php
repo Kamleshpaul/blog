da5744bcbe23a1a9b8a0c8f61d1d362b89877d24
appcloud <kamlesh-php@webappclouds.com>
2019-10-31 17:07:44 +0530

    vuejs added

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="app">
+        <main-app></main-app>
+    </div>
+</body>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/{any}', 'SpaController@index')->where('any', '.*');
