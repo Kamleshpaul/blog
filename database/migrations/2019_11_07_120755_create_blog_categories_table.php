ad77a9cc22e81dc848624338ca4a2a0158cddbd5
appcloud <kamlesh-php@webappclouds.com>
2019-11-07 17:46:51 +0530

    migration and model added

---
+++ database/migrations/2019_11_07_120755_create_blog_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug', 512)->unique();
+            $table->string('name', 256);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_categories');
+    }
+}
---
+++ database/migrations/2019_11_07_120816_create_blogs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug', 512)->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('blog_category_id');
+
+            $table->string('title',256);
+            $table->longText('body');
+            $table->string('status',32);
+            $table->integer('view_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
