6be1d5148b71bfd67e4b2202b9a7184b4839718a
appcloud <kamlesh-php@webappclouds.com>
2019-11-13 14:20:33 +0530

    pwa

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog</title>
+    <link rel="manifest" href="manifest.json">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 </head>
@@ -15,6 +16,7 @@
 <script src="{{ asset('js/jquery.min.js') }}"></script>
 <script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
 <script src="{{ asset('js/app.js') }}"></script>
+<script src="service-worker.js"></script>
 
 <script>
     window.baseUrl = "{!! url('/') !!}";
